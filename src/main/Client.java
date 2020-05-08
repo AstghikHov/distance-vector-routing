c62f7f4e96451103502adec99e7eae6a15408772
AstghikHov <astghik.hovhannisyan.828@my.csun.edu>
2020-05-08 12:06:19 -0700

    seems to be working

---
+++ src/main/Client.java
@@ -0,0 +1,26 @@
+package main;
+
+
+public class Client {
+    private int id;
+    private String address;
+    private int port;
+    
+    public Client(int ID, String ipAddress, int destPort) {
+        this.id = ID;
+        address = ipAddress;
+        port = destPort;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public int getPort() {
+        return port;
+    }
+}
---
+++ src/main/Node.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Node {
+    public String ip;
+    public String port;
+    public int id;
+    
+    public Node(String s, String p) {
+        this.ip = s;
+        this.port = p;
+    }
+    
+    public Node(){}
+}
